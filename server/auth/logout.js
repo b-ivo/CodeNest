ba1a6a04325587018a5e6b6bced2af723f99bca3
b-ivo <ivobryan12@gmial.com>
2025-10-20 16:45:34 +0200

    Added Logout functionality

---
+++ server/auth/logout.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const router = express.Router();
+
+router.post("logout", (req, res) => {
+  try {
+    res.clearCookie("token", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "strict",
+    });
+
+    res.status(200).json({ message: "Logged out successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Logout failed", error: err.message });
+  }
+});
